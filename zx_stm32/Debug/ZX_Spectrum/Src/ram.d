3257a3d8ce89de530152ce4feffa9b309aed8dc7
Yegor Beloussov <35420517+Gorien@users.noreply.github.com>
2018-01-21 20:37:36 +0600

    Initial commit

--- zx_stm32/Debug/ZX_Spectrum/Src/ram.d
+++ zx_stm32/Debug/ZX_Spectrum/Src/ram.d
@@ -1,76 +1,76 @@
 ZX_Spectrum/Src/ram.o: ../ZX_Spectrum/Src/ram.c \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Inc/stm32f4xx_hal_conf.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Inc/main.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cm4.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmInstr.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/cmsis_gcc.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmFunc.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmSimd.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
- C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Inc/main.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Inc/stm32f4xx_hal_conf.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Inc/stm32f4xx_hal_conf.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Inc/main.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Inc/main.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cm4.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmInstr.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmFunc.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmSimd.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Include/core_cmSimd.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 
-C:/Users/Yegor/workspace/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+C:/Users/Yegor/Documents/GitHub/zx_stm32/zx_stm32/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
